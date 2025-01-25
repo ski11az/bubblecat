b983662c404a2df5033ef85f09cf3b15b46a5467
Elias Jonsson <elle.1999@hotmail.com>
2025-01-25 12:01:38 +0100

    Add bubble steering

--- Assets/Scripts/Bubble.cs
+++ Assets/Scripts/Bubble.cs
@@ -9,6 +9,7 @@ public class Bubble : MonoBehaviour
     [SerializeField] float maxScale = 0.3f;     // Max size for the bubble, original scale 0.13f
     [SerializeField] float scaleChange = 0.01f; // Amount to increase per update
     [SerializeField] float riseStrength = 1.5f;
+    [SerializeField] float steerStrength = 2.0f;
 
     private Joint[] _joints;
     private readonly Vector2[][] _connectedAnchor;
@@ -56,6 +57,8 @@ public class Bubble : MonoBehaviour
             AddUpwardsForceToJoints();
         }
 
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        AddForceToJoints(steerStrength * horizontalMove * Vector2.right);
     }
 
     void IncreaseBubbleSize() {
@@ -68,9 +71,15 @@ public class Bubble : MonoBehaviour
 
     void AddUpwardsForceToJoints() {
         if(bubble.transform.localScale.x < maxScale) {
-            for (int i = 0; i < bubble_bones.Length; i++) {
-                bubble_bones[i].AddForce(riseStrength * Vector2.up);
-            }
+            AddForceToJoints(riseStrength * Vector2.up);
+        }
+    }
+
+    void AddForceToJoints(Vector2 force)
+    {
+        for (int i = 0; i < bubble_bones.Length; i++)
+        {
+            bubble_bones[i].AddForce(force);
         }
     }
 }
