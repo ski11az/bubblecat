1df1bad30b9b879daa2c2ba5d4f536bc4b0da34e
Karolina S <karolina.shi@outlook.com>
2025-01-25 19:37:52 +0100

    Add bubble pop animation

--- Assets/Scripts/BubbleCollision.cs
+++ Assets/Scripts/BubbleCollision.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class BubbleCollision : MonoBehaviour
 {
     [SerializeField] GameObject bubble;
-    [SerializeField] Rigidbody2D[] bubble_bones;
     [SerializeField] GameObject pop;
     [SerializeField] float scaleChange = 0.01f; // Amount to increase per update
+
+    GameObject popAnimation;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -15,21 +16,8 @@ public class BubbleCollision : MonoBehaviour
     }
 
     void PopBubble() {
-        Instantiate(pop, this.transform.position, Quaternion.identity);
-        StartCoroutine(ShrinkBubble());
-    }
-
-    IEnumerator ShrinkBubble()
-    {
-        while (bubble.transform.localScale.x > 0)
-        {
-            Vector3 newScale = bubble.transform.localScale - new Vector3(scaleChange, scaleChange, scaleChange) * Time.deltaTime;
-            bubble.GetComponent<Bubble>().SetBubbleScale(newScale);
-
-            yield return null;
-        }
-
-        bubble.GetComponent<Bubble>().SetBubbleScale(new Vector3(0.01f, 0.01f, 0.01f));
-        bubble.SetActive(false);
+        popAnimation = Instantiate(pop, this.transform.position, Quaternion.identity);
+        popAnimation.transform.localScale = bubble.transform.localScale;
+        Destroy(bubble);
     }
 }
---
+++ Assets/Scripts/CalculateDistance.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CalculateDistance : MonoBehaviour
+{
+    [SerializeField] GameObject point1_gameobject;
+    [SerializeField] GameObject point2_gameobject;
+    [SerializeField] float baseDistance; // Ideal distance when "distance"-value should be 1 (no effects applied)
+
+    private float _distance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distance = GetDistance();
+        Debug.Log(_distance);
+    }
+
+    public float GetDistance() { // Returns distance as a value between 0-1
+        return Vector3.Distance(point1_gameobject.transform.position, point2_gameobject.transform.position) / baseDistance;
+    }
+}
