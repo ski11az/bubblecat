75e3690acbea1c8501a932be88180568d54fc537
Elias Jonsson <elle.1999@hotmail.com>
2025-01-26 11:28:41 +0100

    Add game state events

--- Assets/Scripts/BubbleCollision.cs
+++ Assets/Scripts/BubbleCollision.cs
@@ -26,6 +26,7 @@ public class BubbleCollision : MonoBehaviour
     }
 
     void PopBubble() {
+        Debug.Log("Bubble popped");
         int random = Random.Range(0, audioManager.pop.Length);
         audioManager.PlaySound(audioManager.sfxSource, audioManager.pop[random]);
         audioManager.PlayOneShot(audioManager.sfxSource, audioManager.meowFall[0],1);
@@ -36,7 +37,7 @@ public class BubbleCollision : MonoBehaviour
         dangleCat.gameObject.GetComponent<PlayerTransformFollow>().enabled = false;
         dangleCat.gameObject.GetComponent<HingeJoint2D>().enabled = false;
         Rigidbody2D catRb = dangleCat.gameObject.GetComponent<Rigidbody2D>();
-        catRb.velocity = new Vector2(catRb.velocity.x, 0.0f);
+        catRb.velocity = new Vector2(0.0f, 0.0f);
         dangleCat.GetComponent<CircleCollider2D>().enabled = true;
 
         Destroy(bubble);
