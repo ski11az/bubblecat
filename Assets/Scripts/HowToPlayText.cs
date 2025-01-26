4e83d1625da54961b650550fc3f65c8f8732e148
Karolina S <karolina.shi@outlook.com>
2025-01-26 11:32:10 +0100

    Add how to play scene and script

---
+++ Assets/Scripts/HowToPlayText.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HowToPlayText : MonoBehaviour
+{
+    [SerializeField] GameObject howToPlay;
+    [SerializeField] float textFadeOutSeconds;
+
+    private TMP_Text[] _texts;
+
+    private void Start()
+    {
+        _texts = howToPlay.GetComponentsInChildren<TMP_Text>();
+    }
+
+    public void ShowText()
+    {
+        howToPlay.SetActive(true);
+        foreach(TMP_Text text in _texts)
+        {
+            StartCoroutine(FadeTextToZeroAlpha(textFadeOutSeconds, text));
+        }
+    }
+
+    // Bara för debug purposes
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Hej!");
+            ShowText();
+        }
+    }
+    
+    public IEnumerator FadeTextToZeroAlpha(float time, TMP_Text text)
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1);
+        while (text.color.a > 0.0f)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - (Time.deltaTime / time));
+            yield return null;
+        }
+    }
+}
